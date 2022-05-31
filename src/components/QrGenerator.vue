70826403bceac550ddb2c24b09adc2115ee27471
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-05-31 20:49:01 +0300

    Redesign form

--- src/components/QrGenerator.vue
+++ src/components/QrGenerator.vue
@@ -1,37 +1,66 @@
 <template>
   <div>
-    <div class="row justify-center">
-      <h1>{{ t("qr-code") }}</h1>
+    <div class="row justify-content-center">
+      <h1>{{ t("page_title") }}</h1>
     </div>
 
-     <div class="row justify-center">
-      <LanguageSelector/>
-    </div>
+    <div class="container">
+      <div class="row">
+        <div class="column">
+          <div class="row">
+            <h2>{{ t("title_step1") }}</h2>
+          </div>
+          <div class="row">
+            <LanguageSelector />
+          </div>
 
-    <div class="row justify-center">
-      <input
-        type="text"
-        v-model="scope.phoneNumber"
-        @input="addQrCodeToCanvas"
-        class="phone-input"
-      />
-    </div>
+          <div class="row align-items-center">
+            <div class="form-field">
+              <label for="line2">{{ t("line2_label") }}</label>
+              <input
+                type="text"
+                name="line2"
+                v-model="scope.line2"
+                @input="addQrCodeToCanvas"
+              />
+            </div>
+          </div>
 
-    <div class="row justify-center">
-      <button @click="addQrCodeToCanvas" class="btn">
-        {{ t("generate") }}
-      </button>
-    </div>
+          <div class="row align-items-center">
+            <div class="form-field">
+              <label for="line1">{{ t("line1_label") }}</label>
+              <input
+                type="text"
+                name="line1"
+                v-model="scope.line1"
+                @input="addQrCodeToCanvas"
+              />
+            </div>
+          </div>
 
-    <div class="row justify-center">
-      <div v-show="scope.qrcode" class="preview-container">
-        <canvas
-          ref="canvasResult"
-          class="canvas-draw"
-          width="250"
-          height="250"
-        ></canvas>
-        <button @click="download" class="btn">{{ t("download") }}</button>
+        </div>
+
+        <div
+          class="column hide"
+          :class="{
+            show: scope.hasCanvas,
+          }"
+        >
+          <div class="row">
+            <h2>{{ t("title_step2") }}</h2>
+          </div>
+          <div class="row">
+            <div class="preview-container">
+              <canvas
+                ref="canvasResult"
+                class="canvas-draw"
+                width="250"
+                height="250"
+              ></canvas>
+              <button @click="download" class="btn">{{ t("download") }}</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -44,15 +73,15 @@ import QRCode from 'qrcode';
 import { useI18n } from 'vue-i18n';
 
 const scope = reactive({
-  phoneNumber: '',
-  qrcode: null,
-  ctx: null,
+  line1: '',
+  line2: '',
+  hasCanvas: false,
 });
 const { t } = useI18n();
 const canvasResult = ref(null);
 
 async function addQrCodeToCanvas() {
-  scope.qrcode = await QRCode.toDataURL(scope.phoneNumber);
+  scope.hasCanvas = !!(scope.line1 || scope.line2);
 
   const ctx = canvasResult.value.getContext('2d');
 
@@ -63,30 +92,34 @@ async function addQrCodeToCanvas() {
 
   // create new image and add it to canvas
   const img = new Image(200, 200);
-  img.src = scope.qrcode;
-  img.onload = () => {
-    ctx.drawImage(
-      img,
-      (canvasResult.value.width - img.width) / 2,
-      0,
-      img.width,
-      img.height,
-    );
-  };
+  if (scope.line2) {
+    img.src = await QRCode.toDataURL(scope.line2);
+    img.onload = () => {
+      ctx.drawImage(
+        img,
+        (canvasResult.value.width - img.width) / 2,
+        0,
+        img.width,
+        img.height,
+      );
+    };
+  }
 
-  // add cta to canvas
   ctx.font = '18px Arial';
-  const ctaText = ctx.measureText('Suna-ma');
+
+  // add line1 to canvas
+  const line1Text = ctx.measureText(scope.line1);
   ctx.fillText(
-    t('cta'),
-    (canvasResult.value.width - ctaText.width) / 2,
+    scope.line1,
+    (canvasResult.value.width - line1Text.width) / 2,
     img.height + 18,
   );
 
-  const phoneNumberText = ctx.measureText(scope.phoneNumber);
+  // add line1 to canvas
+  const line2Text = ctx.measureText(scope.line2);
   ctx.fillText(
-    scope.phoneNumber,
-    (canvasResult.value.width - phoneNumberText.width) / 2,
+    scope.line2,
+    (canvasResult.value.width - line2Text.width) / 2,
     img.height + 40,
   );
 }
@@ -94,33 +127,56 @@ async function addQrCodeToCanvas() {
 function download() {
   const fileLink = document.createElement('a');
   fileLink.href = canvasResult.value.toDataURL();
-  fileLink.setAttribute('download', scope.phoneNumber);
+  fileLink.setAttribute('download', scope.line1);
   fileLink.setAttribute('target', '_blank');
   document.body.appendChild(fileLink);
   fileLink.click();
   fileLink.remove();
 }
+
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+
 .row {
   display: flex;
   flex-wrap: nowrap;
   padding: 1rem;
+
+  .column {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex: 1;
+    padding: 1rem;
+  }
 }
 
-.justify-center {
+.justify-content-center {
   justify-content: center;
 }
 
-.phone-input {
-  border: 1px solid #d7d9dd;
-  border-radius: 36px;
-  opacity: 1;
-  padding: 5px;
-  line-height: 1.5;
-  height: 30px;
-  width: 250px;
+.align-items-center {
+  align-items: center;
+}
+
+.form-field {
+  display: flex;
+  flex-direction: column;
+
+  input {
+    border: 1px solid #d7d9dd;
+    border-radius: 36px;
+    opacity: 1;
+    padding: 5px;
+    line-height: 1.5;
+    height: 30px;
+    width: 250px;
+  }
 }
 
 .preview-container {
@@ -148,24 +204,40 @@ function download() {
   min-width: 85px;
   font-weight: 500;
   letter-spacing: 0.0892857143em;
+  flex-grow: 1;
+}
+
+.hide {
+  visibility: hidden;
+  opacity: 0;
+  transition: visibility 0s, opacity 0.5s linear;
+  position: absolute;
+}
+
+.show {
+  visibility: visible;
+  opacity: 1;
+  position: relative;
 }
 </style>
 
 <i18n>
 {
   "en": {
-    "qr-code": "QR Code Parking",
-    "phone-number": "Phone number",
-    "generate": "Generate",
+    "page_title": "QR Code Contact",
     "download": "Download",
-    "cta": "Call me"
+    "line1_label": "Call to action",
+    "line2_label": "Choose the QR Code content",
+    "title_step1": "Step 1: Set the content",
+    "title_step2": "Step 2: Preview"
   },
   "ro": {
-    "qr-code": "QR Code Parking",
-    "phone-number": "Număr de telefon",
-    "generate": "Generează",
+    "page_title": "QR Code Contact",
     "download": "Descarcă",
-    "cta": "Sună-mă"
+    "line1_label": "Call to action",
+    "line2_label": "Alege continutul QR Code",
+    "title_step1": "Pasul 1: Alege continutul",
+    "title_step2": "Pasul 2: Previzualizare"
   },
 }
 </i18n>
